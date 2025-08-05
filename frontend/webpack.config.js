8b0a335cb099d842f6d712cd6f75487b9aee5e78
Costas Ford <costasford@yahoo.com>
2025-08-05 11:11:45 -0700

    overhaul

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,26 +1,33 @@
 const path = require("path");
 
 module.exports = {
-  mode: 'none',
+  mode: 'development',
   context: __dirname,
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    clean: true
   },
   resolve: {
-    extensions: [".js", "*"],
+    extensions: [".js", ".jsx"],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
     ],
   },
-  node: {
-    fs: "empty",
-  },
   devtool: "source-map",
+  optimization: {
+    minimize: false
+  }
 };
